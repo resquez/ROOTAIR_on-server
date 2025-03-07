03c7a80ddf6b6f8ead091a0a230c4cb453289d4b
resquez <83383092+resquez@users.noreply.github.com>
2025-03-07 11:57:49 +0900

    update py file

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ from blueprints.member import User
 from blueprints.utils import get_db_connection
 from flask_login import UserMixin
 from flask_cors import CORS
+from flask_session import Session
+import os
 
 app = Flask(__name__)
-CORS(app)
-
 app.config['SECRET_KEY'] = '1234'
+CORS(app,supports_credentials=True,resources={r"/api/*":{"origins":"http://58.127.241.84:61080"}})
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -43,11 +44,13 @@ mail = Mail(app)
 
 
 class User(UserMixin):
-    def __init__(self, id,user_id, username, password):
+    def __init__(self, user_id, id,isadmin, username, password):
         self.id = id
-        self.user_id=user_id
+        self.user_id = user_id
+        self.isadmin=isadmin
         self.username = username
         self.password = password
+        
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -59,11 +62,18 @@ def load_user(user_id):
     connection.close()
 
     if user_data:
-        return User(id=user_data['id'],user_id=user_data['user_id'], username=user_data['username'], password=user_data['password'])
+        return User(id=user_data['id'], username=user_data['username'],isadmin=user_data['isadmin'], password=user_data['password'], user_id = user_data['user_id'])
     return None
 
+# app.config['SESSION_COOKIE_NAME'] = "cookiethatImade"
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
+app.config['SESSION_COOKIE_HTTPONLY'] = True  # 기본값: True
+app.config['SESSION_COOKIE_SECURE'] = False  # HTTPS 환경에서만 세션 쿠키를 사용하는 경우, False로 설정
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'  # HTTPS 환경에서만 세션 쿠키를 사용하는 경우, False로 설정
+app.config['SESSION_FILE_DIR'] = os.path.join(os.getenv('TEMP'), 'flask_session')
+app.config['SESSION_REFRESH_EACH_REQUEST'] = False
+Session(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from app import app  # app.py에서 Flask 앱을 가져옴
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=True, port=5000)
